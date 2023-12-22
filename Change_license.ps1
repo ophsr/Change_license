03650f91d1f7710ae4d8cbfafb5c59aa4b7c85c5
Pedro Henrique Silva Rodrigues <phsr2001@gmail.com>
2023-12-22 09:47:28 -0300

    first commit-v1.0

---
+++ Change_license.ps1
@@ -0,0 +1,160 @@
+﻿<#
+=============================================================================================
+Name:           Change license Office 365 users
+Author:         Pedro Rodrigues > cast.pedroh.utic@sebrae.com.br | phsr2001@gmail.com | ophsr.com.br
+============================================================================================
+#>
+
+
+Function ConnectMgGraphModule {
+    $MsGraphModule = Get-Module Microsoft.Graph -ListAvailable
+    if ($MsGraphModule -eq $null) { 
+        Write-host "Important: Microsoft Graph PowerShell module is unavailable. It is mandatory to have this module installed in the system to run the script successfully." 
+        $confirm = Read-Host Are you sure you want to install Microsoft graph module? [Y] Yes [N] No  
+        if ($confirm -match "[yY]") { 
+            Write-host "Installing Microsoft Graph PowerShell module..."
+            Install-Module Microsoft.Graph -Scope CurrentUser
+            Write-host "Microsoft Graph PowerShell module is installed in the machine successfully" -ForegroundColor Magenta
+        } 
+        else { 
+            Write-host "Exiting. `nNote: Microsoft Graph PowerShell module must be available in your system to run the script" -ForegroundColor Red
+            Exit 
+        } 
+    }
+
+    Connect-MgGraph -Scopes "User.Read.All", "Group.ReadWrite.All"  -ErrorVariable ConnectionError | Out-Null
+    if ($ConnectionError -ne $null) {    
+        Write-Host $ConnectionError -Foregroundcolor Red
+        Exit
+    }
+    else {
+        Write-Host "Microsoft Graph PowerShell module is connected successfully" -ForegroundColor Green
+    }
+}
+
+function Choice {
+    $ValidInput = @('1', '2')
+    $response = ''
+    $filePath = ''
+
+
+    while ($filePath -eq '') {
+        $filePath = Read-Host "Path of the file containing the users that will be modified (Ex: c:\temp\user1.csv)" 
+        $testpath = Test-Path -Path $filePath -PathType Leaf
+        if (-not $testpath) {
+            [console]::Beep(1000, 100)
+            write-host "$filePath is not valid" -ForegroundColor Red
+            $filePath = ''
+        }
+    }
+    
+    Write-Host "`r`nWhich license do you want to ADD and REMOVE in the list users ($filePath)"-ForegroundColor DarkYellow
+    Write-Host "## 1- ADD M365 E3 and REMOVE Office 365 E3`r`n## 2- ADD M365 F3 and REMOVE Office 365 F3`r`n" -ForegroundColor DarkYellow
+    while ($response -eq '') {
+        $response = Read-Host -Prompt "    Enter option (1 or 2)"
+        if ($response -notin $ValidInput) {
+            [console]::Beep(1000, 100)
+            $response = ''
+        }
+    }
+    Switch ($response) {
+        1 {
+            Write-Log INFO "ADD M365 E3 and REMOVE Office 365 E3"
+            ChangeLicense ChangeForM365_e3 $filePath
+        }
+        2 {
+            Write-Log INFO "ADD M365 F3 and REMOVE Office 365 F3"
+            ChangeLicense ChangeForM365_f3 $filePath
+        }
+    }
+}
+
+function ChangeLicense {
+    Param
+    (
+        [Parameter(Mandatory = $true)][ValidateSet("ChangeForM365_e3", "ChangeForM365_f3")]
+        [string]$Type,
+        [Parameter(Mandatory = $true)][string]$FilePath
+    )
+    $Office_f3 = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'DESKLESSPACK'
+    $Office_e3 = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'ENTERPRISEPACK'
+    $Microsoft_f3 = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_F1'
+    $Microsoft_e3 = Get-MgSubscribedSku -All | Where SkuPartNumber -eq 'SPE_E3'
+    $UpnList = Import-Csv $FilePath ";"
+    $UpnListCount = ($UpnList | Measure-Object).Count
+    $counter = 0
+    foreach ($upn in $UpnList) {
+        $counter++
+        Write-Progress -Activity 'Processing...' -CurrentOperation $upn.UPN -PercentComplete (($counter / $UpnListCount) * 100)
+        if ($Type -eq "ChangeForM365_e3") {
+            #Unassign Office E3
+            Set-MgUserLicense -UserId $upn.UPN -AddLicenses @{} -RemoveLicenses @($Office_e3.SkuId) -ErrorVariable ConnectionError | Out-Null 
+            if ($ConnectionError -ne $null) {    
+                Write-Log ERROR "$upn -> $ConnectionError"
+            }
+            else {
+                Write-Log INFO "$upn - Remove Office E3 successfully" 
+            }
+            #Assign M365 E3
+            Set-MgUserLicense -UserId $upn.UPN -AddLicenses @{SkuId = $Microsoft_e3.SkuId }  -RemoveLicenses @() -ErrorVariable ConnectionError | Out-Null
+            if ($ConnectionError -ne $null) {    
+                Write-Log ERROR "$upn -> $ConnectionError"
+            }
+            else {
+                Write-Log INFO "$upn - Add Microsoft E3 successfully" 
+            }
+        }
+        if ($Type -eq "ChangeForM365_f3") {
+            # Unassign Office f3
+            Set-MgUserLicense -UserId $upn.UPN -AddLicenses @{} -RemoveLicenses @($Office_f3.SkuId) -ErrorVariable ConnectionError | Out-Null 
+            if ($ConnectionError -ne $null) {    
+                Write-Log ERROR "$upn -> $ConnectionError"
+            }
+            else {
+                Write-Log INFO "$upn - Remove Office F3 successfully"
+            }
+            #Assign M365 f3
+            Set-MgUserLicense -UserId $upn.UPN -AddLicenses @{SkuId = $Microsoft_f3.SkuId }  -RemoveLicenses @() -ErrorVariable ConnectionError | Out-Null 
+            if ($ConnectionError -ne $null) {    
+                Write-Log ERROR "$upn -> $ConnectionError"
+            }
+            else {
+                Write-Log INFO "$upn - Add Microsoft F3 successfully" 
+            }
+        }
+        Start-Sleep -Milliseconds 500
+    }
+}
+
+
+Function Write-Log {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $False)]
+        [ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG")]
+        [String]
+        $Level = "INFO",
+
+        [Parameter(Mandatory = $True, ValueFromPipeline)]
+        [string]
+        $Message,
+
+        [Parameter(Mandatory = $False)]
+        [string]
+        $logfile = 'log_change_licence.log'
+    )
+
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $Line = "$Stamp $Level $Message"
+    If ($logfile) {
+        Add-Content $logfile -Value $Line
+    }
+    Else {
+        Write-Output $Line
+    }
+}
+Function main() {
+    ConnectMgGraphModule
+    Choice
+}
+. main
---
+++ README.md
@@ -0,0 +1,126 @@
+## Change License Office 365 Users Script Documentation
+
+### Português
+
+#### Visão Geral
+O script PowerShell fornecido automatiza o processo de alteração de licenças para usuários do Office 365 por meio da API Microsoft Graph. Ele oferece a flexibilidade de escolher entre adicionar as licenças Microsoft 365 (M365) E3 ou F3 e remover as licenças correspondentes do Office 365 para uma lista especificada de usuários em um arquivo CSV.
+
+#### Pré-requisitos
+1. **Módulo PowerShell do Microsoft Graph:**
+   - O script verifica a disponibilidade do módulo PowerShell do Microsoft Graph. Se ausente, solicita que o usuário o instale.
+   - O módulo Microsoft Graph pode ser instalado com o comando: `Install-Module Microsoft.Graph -Scope CurrentUser`.
+  
+2. **Formato do Arquivo CSV**
+   - O arquivo CSV deve conter uma coluna com o cabeçalho 'UPN' (User Principal Name), representando as identidades dos usuários. O script utiliza esta coluna para identificar e modificar as licenças dos usuários.
+
+3. **Autenticação do Usuário**
+   - Quando o comando Connect-MgGraph é executado, o script solicita que o usuário faça login no Microsoft Graph. O usuário precisa inserir suas credenciais para estabelecer a conexão.
+
+#### Funções do Script
+
+##### Função `ConnectMgGraphModule`
+- Verifica a presença do módulo PowerShell do Microsoft Graph e o instala, se necessário.
+- Conecta-se ao Microsoft Graph com as permissões necessárias.
+
+##### Função `Choice`
+- Solicita ao usuário o caminho do arquivo CSV contendo a lista de usuários a serem modificados.
+- Permite ao usuário escolher entre duas opções de modificação de licença:
+  - Opção 1: ADICIONAR M365 E3 e REMOVER Office 365 E3
+  - Opção 2: ADICIONAR M365 F3 e REMOVER Office 365 F3
+- Chama a função `ChangeLicense` com base na escolha do usuário.
+
+##### Função `ChangeLicense`
+- Modifica a licença para cada usuário no arquivo CSV especificado.
+- Desassocia a licença atual do Office 365 e associa a licença Microsoft 365 correspondente com base na opção selecionada.
+
+##### Função `Write-Log`
+- Registra eventos com carimbos de data/hora, níveis de log e mensagens personalizadas.
+- As mensagens de log são anexadas a um arquivo de log (`log_change_licence.log` por padrão) ou exibidas no console.
+
+##### Função `main`
+- Orquestra a execução do script, chamando as funções `ConnectMgGraphModule` e `Choice`.
+
+#### Uso
+1. **Executar o Script:**
+   - Execute o script no PowerShell ISE
+
+2. **Interação:**
+   - Ao se conectar ao Microsoft Graph, o usuário deve fazer login usando suas credenciais.
+   - O script solicita ao usuário que informe o caminho do arquivo CSV e escolha uma opção de modificação de licença.
+
+3. **Registros:**
+   - Os eventos são registrados em um arquivo de log (`log_change_licence.log` por padrão).
+
+4. **Observação:**
+   - Certifique-se de que o módulo PowerShell do Microsoft Graph esteja instalado para uma execução bem-sucedida do script.
+
+#### Exemplo
+    .\ChangeLicenseScript.ps1
+
+#### Informações adicionais
+    - Author: Pedro Rodrigues
+    - Contact: cast.pedroh.utic@sebrae.com.br | phsr2001@gmail.com | ophsr.com.br
+
+---
+<div style="page-break-after: always;"></div>
+
+### English
+
+#### Overview
+The provided PowerShell script automates the process of changing licenses for Office 365 users through the Microsoft Graph API. It offers the flexibility to choose between adding Microsoft 365 (M365) E3 or F3 licenses and removing the corresponding Office 365 licenses for a specified list of users in a CSV file.
+
+#### Prerequisites
+1. **Microsoft Graph PowerShell Module:**
+   - The script checks for the availability of the Microsoft Graph PowerShell module. If not present, it prompts the user to install it.
+   - The Microsoft Graph module can be installed by running: `Install-Module Microsoft.Graph -Scope CurrentUser`.
+
+2. **CSV File Format**
+   - The CSV file should contain a column with the header 'UPN' (User Principal Name), representing the user identities. The script uses this column to identify and modify user licenses.
+
+3. **User Authentication**
+   - When the Connect-MgGraph command is executed, the script prompts the user to log in to Microsoft Graph. The user needs to enter their credentials to establish the connection.
+
+#### Script Functions
+
+##### `ConnectMgGraphModule` Function
+- Checks for the presence of the Microsoft Graph PowerShell module and installs it if necessary.
+- Connects to Microsoft Graph with the required scopes.
+
+##### `Choice` Function
+- Prompts the user to provide the path of the CSV file containing the list of users to be modified.
+- Allows the user to choose between two license modification options:
+  - Option 1: ADD M365 E3 and REMOVE Office 365 E3
+  - Option 2: ADD M365 F3 and REMOVE Office 365 F3
+- Calls the `ChangeLicense` function based on the user's choice.
+
+##### `ChangeLicense` Function
+- Modifies the license for each user in the specified CSV file.
+- Unassigns the current Office 365 license and assigns the corresponding Microsoft 365 license based on the selected option.
+
+##### `Write-Log` Function
+- Logs events with timestamps, log levels, and custom messages.
+- Log messages are appended to a log file (`log_change_licence.log` by default) or displayed in the console.
+
+##### `main` Function
+- Orchestrates the execution of the script by calling `ConnectMgGraphModule` and `Choice` functions.
+
+#### Usage
+1. **Run the Script:**
+   - Run the script on PowerShell ISE 
+
+2. **Interaction:**
+   - When connecting to Microsoft Graph, the user must log in using their credentials.
+   - The script prompts the user to enter the path of the CSV file and choose a license modification option.
+3. **Logging:**
+   - Events are logged to a log file (`log_change_licence.log` by default).
+
+4. **Note:**
+   - Ensure that the Microsoft Graph PowerShell module is installed for successful script execution.
+
+#### Example
+    .\ChangeLicenseScript.ps1
+
+#### Additional Information
+    - Author: Pedro Rodrigues
+    - Contact: cast.pedroh.utic@sebrae.com.br | phsr2001@gmail.com | ophsr.com.br
+
